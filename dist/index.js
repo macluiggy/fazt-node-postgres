4396d0de20506639620a978af7ffa704030a8ed1
luiggy macias <ferrinluiggy@gmail.com>
2022-03-28 15:12:51 -0500

    fdf

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
+const hello = "Hello World";
+const viteNodeApp = hello;
+exports.viteNodeApp = viteNodeApp;
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// import app from "./app";
+// // import config from "./config/config";
+// // const { port } = config;
+// let port = process.env.PORT || 3000;
+
+// if (process.env.NODE_ENV !== 'dev') {
+//   app.listen(port, (/*err*/) => {
+//     // if (err) {
+//     //   console.log(err);n
+//     // }
+//     console.log(`Listening on port ${port}`);
+//   });
+// }
+ 
+// export const viteNodeApp = app;
+// export default app;
+// // babel-node --extensions \".ts\" index.ts
+// // webpack --mode=development --config ./webpack.config.js && node ./dist/bundle.js
+ 
