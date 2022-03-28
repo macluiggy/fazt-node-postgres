476b186f104350d8412e2f186d7ec0b6312de5a6
luiggy macias <ferrinluiggy@gmail.com>
2022-03-28 17:34:29 -0500

    ffff

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
 
+// routes
+import projectsRouter from './routes/projects.route';
+import tasksRouter from './routes/tasks.route';
+
+// initialize express app
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json())
+
+app.use(projectsRouter)
+app.use(tasksRouter)
